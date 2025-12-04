c1ab6ce2f2773d805f585bf15a415949550ca082
Theofilo Henrique <theohenriquee01@gmail.com>
2025-12-03 22:20:47 -0300

    Refatorando Rotas e Controller de Turma

--- app/Http/Controllers/TurmaController.php
+++ app/Http/Controllers/TurmaController.php
@@ -6,29 +6,85 @@ use Illuminate\Http\Request;
 
 class TurmaController extends Controller
 {
-    function cadastro(){
-        $turma = Turma::create(["name"=>"Theo","codigo"=>"69","curso"=>"ADS"]);
-        dd($turma);
+    public function listar()
+    {
+        $turmas = Turma::all()->toArray();
+        return response()->json($turmas);
     }
 
-    function listar(){
-        $turma = Turma::get()->toArray();
-        dd($turma);
+    public function listarPorId($id)
+    {
+        $turma = Turma::where('id', $id)->first();
+
+        if (!$turma) {
+            return response()->json(['error' => 'Turma não encontrada'], 404);
+        }
+
+        return response()->json($turma->toArray());
     }
 
-    function listarId($id){
-        $turma = Turma::where('id',$id)->get()->toArray();
-        dd($turma);
+    public function criar(Request $request)
+    {
+        $turma = Turma::create([
+            "name" => $request->input('name'),
+            "codigo" => $request->input('codigo'),
+            "curso" => $request->input('curso')
+        ]);
+
+        return response()->json($turma->toArray(), 201);
     }
 
-    function alterar($id,$name){
-        $turma = Turma::where('id',$id)->update(["name"=>$name]);
-        dd($turma);
+    public function atualizar(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'codigo' => 'sometimes|string|unique:turmas,codigo,' . $id,
+            'curso' => 'sometimes|string|max:255'
+        ]);
+
+        $turma = Turma::find($id);
+
+        if (!$turma) {
+            return response()->json(['error' => 'Turma não encontrada'], 404);
+        }
+
+        $turma->fill($validated);
+
+        if (!$turma->isDirty()) {
+            return response()->json([
+                'message' => 'Nenhuma alteração necessária',
+                'turma' => $turma->toArray()
+            ]);
+        }
+
+        $turma->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Turma atualizada com sucesso',
+            'turma' => $turma->toArray()
+        ]);
     }
 
-    function deletar($id){
-        $turma = Turma::where('id',$id)->delete();
-        dd($turma);
+    public function deletar($id)
+    {
+        $turma = Turma::find($id);
+
+        if (!$turma) {
+            return response()->json(['error' => 'Turma não encontrada'], 404);
+        }
+
+        $turmaNome = $turma->name;
+        $turma->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => "Turma '{$turmaNome}' deletada com sucesso",
+            'deleted_data' => [
+                'id' => $id,
+                'name' => $turmaNome,
+                'codigo' => $turma->codigo
+            ]
+        ], 200);
     }
 }
-
